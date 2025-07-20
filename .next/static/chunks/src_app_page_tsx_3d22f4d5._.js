1c27a404fb9e6ea71da75481ea28b46e298e9bb4
zupersoft-web <zupersoft@gmail.com>
2025-07-20 12:10:44 +0800

    Add .next/static/chunks/src_app_page_tsx_3d22f4d5._.js 1752984644065

---
+++ .next/static/chunks/src_app_page_tsx_3d22f4d5._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@radix-ui_e0947dc3._.js"
+],
+    source: "dynamic"
+});
